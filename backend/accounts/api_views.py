470180cfde833eb3eb04fc2078f251f4d5dabe1c
Jan van Hest <janvanhest@outlook.com>
2026-02-19 21:25:23 +0100

    refactor(api_views): remove unused import of User model and clean up views.py by eliminating unnecessary render import

--- backend/accounts/api_views.py
+++ backend/accounts/api_views.py
@@ -6,7 +6,6 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import User
 from .serializers import UserSerializer
 
 
