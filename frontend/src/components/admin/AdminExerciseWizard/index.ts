d24706f430afe12b657883df17c4d6f933e4801b
Jan van Hest <janvanhest@outlook.com>
2026-02-23 09:23:58 +0100

    feat(admin-exercise-wizard): export additional wizard components for enhanced functionality in exercise creation

---
+++ frontend/src/components/admin/AdminExerciseWizard/index.ts
@@ -0,0 +1,17 @@
+export { ExistingVerbDialog } from './ExistingVerbDialog'
+export { Step1Infinitive } from './Step1Infinitive'
+export { Step2Forms } from './Step2Forms'
+export { Step3NoVerbCard } from './Step3NoVerbCard'
+export { Step3Sentences } from './Step3Sentences'
+export { Step4SummaryAndExercise } from './Step4SummaryAndExercise'
+export type {
+  UseAdminGenerateExerciseWizardResult,
+  WizardExistingVerbDialogProps,
+  WizardSentenceDialogsProps,
+  WizardStepperProps,
+  WizardStep1Props,
+  WizardStep2Props,
+  WizardStep3Props,
+  WizardStep4Props,
+} from './types'
+export { WizardSentenceDialogs } from './WizardSentenceDialogs'
--- frontend/src/components/admin/index.ts
+++ frontend/src/components/admin/index.ts
@@ -1,4 +1,11 @@
 export { AdminActionCard } from './AdminActionCard'
 export { AdminActionsSection } from './AdminActionsSection'
+export { AdminExerciseWizardStepper } from './AdminExerciseWizardStepper'
 export { AdminStatCard } from './AdminStatCard'
 export { AdminStatsSection } from './AdminStatsSection'
+export {
+  Step1Infinitive,
+  Step2Forms,
+  Step3Sentences,
+  Step4SummaryAndExercise,
+} from './AdminExerciseWizard'
